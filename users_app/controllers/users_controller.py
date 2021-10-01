1caca0bb4c1d258105f805bc08ceb76cbd45fb7d
Your Name <you@example.com>
2021-10-01 09:58:18 -0600

    files

--- users_app/controllers/users_controller.py
+++ users_app/controllers/users_controller.py
@@ -1,13 +1,16 @@
+from types import new_class
 from flask import render_template, request, redirect
+from flask.globals import session
 
 from users_app import app
+
 from users_app.models.User import User
 
 
 
 @app.route( "/user", methods=['GET'] )
 def displayUsers():
-    return render_template( "home.html", users= User.get_all_users())
+    return render_template( "home.html", users=User.get_all_users())
 
 @app.route( "/user/new", methods=['GET'] )
 def displayForm():
@@ -17,15 +20,56 @@ def displayForm():
 @app.route( "/user/new/add", methods=['POST'] )
 def addUser():
     print(0)
+    id=['id']
     first_name =request.form['first_name']
     last_name = request.form['last_name']
     email= request.form['email']
     created_at=['created_at']
     updated_at=['updated_at']
-    newUser = User( first_name,last_name,email,created_at,updated_at)
+    newUser = User(id,first_name, last_name, email, created_at, updated_at)
     result = User.add_user(newUser)
     print( result )
     return redirect('/user')
 
+
+@app.route('/user/edit/<id>')
+def edit(id):
+    data ={ 
+        "id":int(id)
+    }
+    newUser=User(data)
+    result=User.get_one(newUser)
+    return render_template("edit.html")
+
+@app.route('/user/show/<id>',methods=['GET'])
+def show(id):
+    data ={ 
+        "id":int(id),
+       
+        
+    }
+    
+    return render_template("show.html",user=User.get_one_user(data))
+
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    id=['id']
+    first_name =request.form['first_name']
+    last_name = request.form['last_name']
+    email= request.form['email']
+    created_at=['created_at']
+    updated_at=['updated_at']
+    newUser = User( id,first_name,last_name,email,created_at,updated_at)
+    result = User.update(newUser)
+    return result,redirect('/user')
+
+@app.route('/user/delete/<id>')
+def delete(id):
+    data ={
+        "id": int(id)
+    }
+    User.delete(data)
+    return redirect('/user')
 
 
--- users_app/models/User.py
+++ users_app/models/User.py
@@ -3,32 +3,43 @@ from users_app.config.MySQLConnection import connectToMySQL
 from users_app.models import User
 
 class User:
-    def __init__( self, first_name, last_name, email,created_at,updated_at):
+    def __init__( self, id,first_name, last_name, email,created_at,updated_at):
+        self.id=id
         self.first_name = first_name
         self.last_name = last_name
         self.email= email
         self.created_at=created_at
         self.updated_at=updated_at
-        
-        
-      
-        
-
-
+   
     @classmethod
     def get_all_users( cls ):
         query = "SELECT * FROM users;"
-     
-        
         results = connectToMySQL( "users" ).query_db( query )
         users = []
         for user in results:
-            users.append( User( user['first_name'], user['last_name'], user['email'],user['created_at'],user['updated_at']))
+            users.append( User( user['id'],user['first_name'], user['last_name'], user['email'],user['created_at'],user['updated_at']))
         return users
     
     @classmethod
     def add_user( cls, newUser ):
-        query = "INSERT INTO users(first_name,last_name,email,updated_at,created_at) VALUES(%(first_name)s, %(last_name)s,%(email)s,now(),now());"
+        query = "INSERT INTO users(first_name,last_name,email,updated_at,created_at) VALUES(%(first_name)s,%(last_name)s,%(email)s,NOW(),NOW());"
+        data = {
+         
+            "first_name" : newUser.first_name,
+            "last_name" : newUser.last_name,
+            "email": newUser.email,
+            "created_at": newUser.created_at,
+            "updated_at": newUser.updated_at,
+    
+                }
+        print(1)
+        result = connectToMySQL( 'users' ).query_db( query, data )
+        print(2)
+        return result
+    
+    @classmethod
+    def update (cls, newUser):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,created_at=now(),updated_at=now());"
         data = {
             "first_name" : newUser.first_name,
             "last_name" : newUser.last_name,
@@ -37,6 +48,24 @@ class User:
             "updated_at": newUser.updated_at
                 }
         print(1)
-        result = connectToMySQL( 'users' ).query_db( query, data )
+        result = connectToMySQL( "users" ).query_db( query, data )
         print(2)
-        return result  
+        return result 
+    
+    @classmethod
+    def delete( cls, id ):
+        query = "DELETE FROM users WHERE id=%(id)s;"
+        data=id
+        result = connectToMySQL( "users" ).query_db( query, data )
+        return result
+    
+    @classmethod
+    def get_one_user(cls,id):
+        query  = "SELECT * FROM users WHERE id=%(id)s;"
+        data=id   
+        result = connectToMySQL("users").query_db(query,data)
+        print(result)
+        print("classmethod")
+        
+        return result
+ 
